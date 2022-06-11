7e4bae40e14e61d78c23020cce5602af5a8746f9
ali <comdarici@gmail.com>
2022-06-11 23:57:55 +0200

    darici

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,75 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+    @FindBy(xpath = "//input[@type='email'][1]")
+    public WebElement loginEmailAddress;
+    @FindBy(xpath = "//input[@type='password'][1]")
+    public WebElement loginPassword;
+    @FindBy(xpath = "//button[@type='submit'][1]")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//input[@data-qa='signup-name'][1]")
+    public WebElement signupName;
+    @FindBy(xpath = "//input[@data-qa='signup-email'][1]")
+    public WebElement signupEmailAddress;
+    @FindBy(xpath = "//button[@data-qa='signup-button'][1]")
+    public WebElement signupButton;
+
+
+    @FindBy(xpath = "//b[contains(text(),'Enter Account Information')]")
+    public WebElement accountInformationText;
+    @FindBy(xpath = "  //label[@for='id_gender1']")
+    public WebElement signupMrCheckBox;
+    @FindBy(xpath = "  //label[@for='id_gender2']")
+    public WebElement signupMrsCheckBox;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement signupPassword;
+    @FindBy(xpath = "//select[@id='days']")
+    public WebElement signupDays;
+    @FindBy(xpath = "//select[@id='months']")
+    public WebElement signupMonths;
+    @FindBy(xpath = "//select[@id='years']")
+    public WebElement signupYears;
+    @FindBy(xpath = "//input[@id='newsletter']")
+    public WebElement signupNewsletter;
+    @FindBy(xpath = "//input[@id='optin']")
+    public WebElement  specialOffersCheckBox;
+
+
+    @FindBy(xpath = "//input[@id='first_name']")
+    public WebElement  signupFirstName;
+    @FindBy(xpath = "//input[@id='last_name']")
+    public WebElement  signupLastName;
+    @FindBy(xpath = "//input[@id='company']")
+    public WebElement  SignupCompanyName;
+    @FindBy(xpath = " //input[@id='address1']")
+    public WebElement  signupAddress1;
+    @FindBy(xpath = " //input[@id='address2']")
+    public WebElement  signupAddress2;
+    @FindBy(xpath = "//select[@id='country']")
+    public WebElement  signupCountryDropdown;
+    @FindBy(xpath = "//input[@id='state']")
+    public WebElement  signupState;
+
+    @FindBy(xpath = "//input[@id='city']")
+    public WebElement  signupCity;
+    @FindBy(xpath = "//input[@id='zipcode']")
+    public WebElement  signupZipcode;
+    @FindBy(xpath = "//input[@id='mobile_number']")
+    public WebElement  signupMobileNumber;
+    @FindBy(xpath = "//button[@data-qa='create-account']")
+    public WebElement  signupCreateAccountButton;
+
+
 }
