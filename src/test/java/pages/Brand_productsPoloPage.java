3b5cdad9b9080f28efa026f3dbcd19fc98ee86d2
NaciyeAydin <naciyeaydinnn@gmail.com>
2022-06-13 10:16:30 +0200

    first commit

--- src/test/java/pages/Brand_productsPoloPage.java
+++ src/test/java/pages/Brand_productsPoloPage.java
@@ -33,34 +33,6 @@ public class Brand_productsPoloPage {
     @FindBy(className = "btn btn-success chose-modal btn-block")
     public WebElement continueShoppingButton;
 
-    @FindBy(partialLinkText ="product_details/8")
-    public WebElement ViewProductItemFGT;
 
-    @FindBy(className ="btn btn-default add-to-cart")
-    public WebElement AddToCarItemFGT;
-
-    @FindBy(partialLinkText ="product_details/29")
-    public WebElement ViewProductItemnGreen;
-
-    @FindBy(className ="btn btn-default add-to-cart")
-    public WebElement AddToCarItemGreen;
-
-    @FindBy(partialLinkText ="product_details/30")
-    public WebElement ViewProductItemPT;
-
-    @FindBy(className ="btn btn-default add-to-cart")
-    public WebElement AddToCarItemPT;
-
-    @FindBy(partialLinkText ="product_details/33")
-    public WebElement ViewProductItemJ;
-
-    @FindBy(className ="btn btn-default add-to-cart")
-    public WebElement AddToCarItemJ;
-
-    @FindBy(partialLinkText ="product_details/37")
-    public WebElement ViewProductItemFJ;
-
-    @FindBy(className ="btn btn-default add-to-cart")
-    public WebElement AddToCarItemFJ;
 
 }
