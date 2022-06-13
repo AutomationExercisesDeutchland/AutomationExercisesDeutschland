6210dabd7f46e24bcb9cc693a66c9c52cb4a3e44
yavuz <yavuzuluturk@gmail.com>
2022-06-14 01:53:34 +0200

    delete account ve homepage sayfaları eklendi

--- src/test/java/pages/Delete_account.java
+++ src/test/java/pages/Delete_account.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Delete_account {
+
+    public Delete_account(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    @FindBy(linkText = " Delete Account")
+    public WebElement deleteAccountButton;
+
 }
