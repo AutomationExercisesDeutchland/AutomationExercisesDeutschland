066ea20f6b5943fd4eb171ac3ae16d8093c4cfac
Mustafa Gecer <h.musti.27@gmail.com>
2022-06-14 10:16:53 +0200

    mustafa

--- src/test/java/utilities/ApiUtils.java
+++ src/test/java/utilities/ApiUtils.java
@@ -4,8 +4,7 @@ package utilities;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import pojos.Room;
-import pojos.RoomPost;
+
 
 import static io.restassured.RestAssured.given;
 
@@ -39,39 +38,9 @@ public class ApiUtils {
         return  response;
     }
 
-    public static Response postRequestRoom(String token, String endpoint, RoomPost room) throws Exception {
-
-        ObjectMapper obj=new ObjectMapper();
 
 
-        Response response = given().headers(
-                "Authorization",
-                "Bearer " + token,
-                "Content-Type",
-                ContentType.JSON,
-                "Accept",
-                ContentType.JSON).body(obj.writeValueAsString(room)).when().post(endpoint);
-        return  response;
 
 
-    }
-
-
-    public static Response putRequestRoom(String token, String endpoint,Room room) throws Exception {
-
-        ObjectMapper obj=new ObjectMapper();
-
-
-        Response response = given().headers(
-                "Authorization",
-                "Bearer " + token,
-                "Content-Type",
-                ContentType.JSON,
-                "Accept",
-                ContentType.JSON).body(obj.writeValueAsString(room)).when().put(endpoint);
-        return  response;
-
-
-    }
 
 }
