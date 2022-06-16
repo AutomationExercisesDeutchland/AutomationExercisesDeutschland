a4f9a15930473ad1bf668e91a4cd1e2b4e57e87a
adarici <88286697+adarici@users.noreply.github.com>
2022-06-16 23:12:26 +0200

    Update US_2_Login_User.java

--- src/test/java/stepdefinations/US_2_Login_User.java
+++ src/test/java/stepdefinations/US_2_Login_User.java
@@ -14,7 +14,7 @@ public class US_2_Login_User {
     }
 
     @Then("Verify that Logged in as username is visible as {string}")
-    public void verify_that_is_visible_as_ali(String string, String userName) {
+    public void verify_that_is_visible_as_ali(String userName) {
         Assert.assertEquals(userName,allPages.loginPage().loginNameConfirmationText.getText());
     }
 
